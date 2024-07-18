787b2483360880c16d532451e2f987a51418b1f8
fewlio-LLUNA <seraja0824ryoiki@gmail.com>
2024-07-18 17:01:45 +0900

    fix: cors設定

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "https://fewlio-lluna.github.io/Vaddict_Graph/", // 許可するオリジン
+  "Access-Control-Allow-Methods": "GET", // 許可するメソッド
+  "Access-Control-Allow-Headers": "Content-Type", // 許可するリクエストヘッダー
+};
+
 export const GET = async (req: NextRequest) => {
-    const playerId = req.nextUrl.searchParams.get('player_id')
+  const playerId = req.nextUrl.searchParams.get("player_id");
 
-    const request = await fetch(`https://vaddict.b35.jp/history.php?player_id=${playerId}&item1=pl_played_count&item2=pl_volforce&order=asc`)
+  const request = await fetch(
+    `https://vaddict.b35.jp/history.php?player_id=${playerId}&item1=pl_played_count&item2=pl_volforce&order=asc`
+  );
 
-    const text = await request.text()
+  const text = await request.text();
 
-    // console.log(text);
-    
-    return NextResponse.json({text: text}, {status: 200})
-}
+  // console.log(text);
+
+  return NextResponse.json({ text: text }, { status: 200 });
+};
